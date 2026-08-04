bc9354b19546a4e409bea2391a844c9b376dc399
urMisery <urmiseryukr@gmail.com>
2026-08-04 21:30:16 +0200

    Chapter 1 - Basics

---
+++ Chapter-01-Basics/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int age {33};
+    double weight {81.23};
+    cout << "Name: " << "Max" << "\n\t" << "Age: " << age << "\n\t\t" << "Weight: " << weight <<  endl;
+}
